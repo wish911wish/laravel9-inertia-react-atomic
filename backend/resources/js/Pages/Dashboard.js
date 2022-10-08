c6f285aa7bde692294d20a0f38b868dd72ef09bc
wish911wish <wish911wish@gmail.com>
2022-10-08 15:33:14 +0900

    use button component

--- backend/resources/js/Pages/Dashboard.js
+++ backend/resources/js/Pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Authenticated from '@/Layouts/Authenticated';
 import { Head } from '@inertiajs/inertia-react';
+import Button, { PrimaryButton, WarningButton } from '@/Components/Atoms/Button';
 
 export default function Dashboard(props) {
     return (
@@ -15,6 +16,9 @@ export default function Dashboard(props) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 bg-white border-b border-gray-200">You're logged in!</div>
+                        <Button>aaaa</Button>
+                        <PrimaryButton>bbbb</PrimaryButton>
+                        <WarningButton>cccc</WarningButton>
                     </div>
                 </div>
             </div>
