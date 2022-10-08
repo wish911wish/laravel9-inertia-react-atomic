794b7fb0b966b95a9af27403f3ee6bef4be0cf47
wish911wish <wish911wish@gmail.com>
2022-10-08 15:32:47 +0900

    make button components

---
+++ backend/resources/js/Components/Atoms/Button/index.tsx
@@ -0,0 +1,17 @@
+import { ComponentPropsWithoutRef } from "react"
+import styles from "./styles.module.css";
+import clsx from "clsx";
+
+type Type = 'default' | 'primary' | 'warning';
+type Props = ComponentPropsWithoutRef<'button'>
+
+
+const buttonFactory = (type: Type) => ({ children, ...props }: Props) => (
+  <button { ...props } className={clsx(styles.button, styles[type])}>{ children }</button>
+)
+
+export const Button = buttonFactory('default');
+export const PrimaryButton = buttonFactory('primary');
+export const WarningButton = buttonFactory('warning');
+
+export default Button;
