5dce428a9bbfce44c5daab21530de097f84e38b0
wish911wish <wish911wish@gmail.com>
2022-10-08 16:33:18 +0900

    add story book

---
+++ backend/.storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    "../resources/js/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react",
+  addons: [
+    "storybook-css-modules",
+  ],
+}
---
+++ backend/resources/js/Components/Atoms/Button/index.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import Button, {PrimaryButton, WarningButton} from "./index";
+
+type T = typeof Button;
+type Story = ComponentStoryObj<T>;
+
+export default {
+  component: Button,
+  args: { children: "送信する" },
+} as ComponentMeta<T>;
+
+export const Default: Story = {};
+export const Primary = () => <PrimaryButton>送信する</PrimaryButton>;
+export const Warning = () => <WarningButton>送信する</WarningButton>;
