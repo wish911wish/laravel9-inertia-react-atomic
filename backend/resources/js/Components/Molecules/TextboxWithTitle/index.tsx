1485381659ec6453a44937ab289c971bddb587eb
wish911wish <wish911wish@gmail.com>
2022-10-23 16:54:31 +0900

    fix TextBoxWithTitle

--- backend/resources/js/Components/Molecules/TextboxWithTitle/index.tsx
+++ backend/resources/js/Components/Molecules/TextboxWithTitle/index.tsx
@@ -1,25 +1,27 @@
-import TextBox from '../../Atoms/TextBox'
-import Txt, {WarningTxt} from '../../Atoms/Txt'
-import { ComponentPropsWithoutRef, ComponentPropsWithRef } from "react"
-import styles from "./styles.module.css";
-import clsx from "clsx";
+import { TextBox } from '../../Atoms/TextBox'
+import Txt, { WarningTxt } from '../../Atoms/Txt'
+import { ComponentPropsWithoutRef, ComponentPropsWithRef } from 'react'
+import styles from './styles.module.css'
+import clsx from 'clsx'
 
 type Props = {
-  className?: string;
-  titleLevel?: 3 | 4 | 5 | 6;
-  labelProps: Omit<ComponentPropsWithoutRef<"label">, "htmlFor" | "className">;
-  textboxProps: Omit<ComponentPropsWithRef<"input">, "id">;
-  description?: string;
-  error?: string;
-};
+  className?: string
+  titleLevel?: 3 | 4 | 5 | 6
+  type?: string
+  labelProps: Omit<ComponentPropsWithoutRef<'label'>, 'htmlFor' | 'className'>
+  textboxProps?: Omit<ComponentPropsWithRef<'input'>, 'id'>
+  description?: string
+  error?: string
+}
 
 const TextBoxWithTitle = ({
   className,
   titleLevel = 3,
+  type = 'text',
   labelProps: { children, ...labelProps },
   textboxProps,
   description,
-  error
+  error,
 }: Props) => {
   return (
     <div className={clsx(className, styles.module)}>
@@ -27,22 +29,14 @@ const TextBoxWithTitle = ({
         <span role="heading" aria-level={titleLevel} className={styles.title}>
           {children}
         </span>
-        <TextBox
-          {...textboxProps}
-          className={clsx(textboxProps?.className, styles.textbox)}
-          aria-invalid={!!error}
-        />
+        <TextBox {...textboxProps} className={clsx(textboxProps?.className, styles.textbox)} type={type} aria-invalid={!!error} />
       </label>
       <div className={styles.bottom}>
-        {description && (
-          <Txt>{description}</Txt>
-        )}
-        {error && (
-          <WarningTxt className={styles.error}>{error}</WarningTxt>
-        )}
+        {description && <Txt>{description}</Txt>}
+        {error && <WarningTxt className={styles.error}>{error}</WarningTxt>}
       </div>
     </div>
   )
-};
+}
 
-export default TextBoxWithTitle;
+export default TextBoxWithTitle
