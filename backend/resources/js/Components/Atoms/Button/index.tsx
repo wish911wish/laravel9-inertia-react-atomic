74345bf6ca4a02fef7ad87c1691fb3d51752f996
wish911wish <wish911wish@gmail.com>
2022-10-23 16:53:05 +0900

    fix button components

--- backend/resources/js/Components/Atoms/Button/index.tsx
+++ backend/resources/js/Components/Atoms/Button/index.tsx
@@ -1,17 +1,21 @@
-import { ComponentPropsWithoutRef } from "react"
-import styles from "./styles.module.css";
-import clsx from "clsx";
+import { ComponentPropsWithoutRef } from 'react'
+import styles from './styles.module.css'
+import clsx from 'clsx'
 
-type Type = 'default' | 'primary' | 'warning';
+type Type = 'default' | 'primary' | 'warning'
 type Props = ComponentPropsWithoutRef<'button'>
 
+const buttonFactory =
+  (type: Type) =>
+  ({ children, ...props }: Props) =>
+    (
+      <button {...props} className={clsx(styles.button, styles[type])}>
+        {children}
+      </button>
+    )
 
-const buttonFactory = (type: Type) => ({ children, ...props }: Props) => (
-  <button { ...props } className={clsx(styles.button, styles[type])}>{ children }</button>
-)
+export const Button = buttonFactory('default')
+export const PrimaryButton = buttonFactory('primary')
+export const WarningButton = buttonFactory('warning')
 
-export const Button = buttonFactory('default');
-export const PrimaryButton = buttonFactory('primary');
-export const WarningButton = buttonFactory('warning');
-
-export default Button;
+export default Button
