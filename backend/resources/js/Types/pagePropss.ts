ca51d7361559db22b6debd6eff2c5fc9d721c8ab
wish911wish <wish911wish@gmail.com>
2022-11-05 10:20:48 +0900

    fix file name

--- backend/resources/js/Types/pagePropss.ts
+++ backend/resources/js/Types/pagePropss.ts
@@ -1,5 +1,5 @@
 import * as Inertia from '@inertiajs/inertia'
-import User from '@/Types/user'
+import { User } from '@/Types/User'
 
 export default interface PageProps extends Inertia.Page<Inertia.PageProps> {
   props: {
